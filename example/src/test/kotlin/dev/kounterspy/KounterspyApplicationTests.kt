c9846f929c4494a7530fddc758ce8c93f8cd8402
Roy Brokvam <roy.brokvam@gmail.com>
2024-01-16 15:38:18 +0100

    Adds dependencies to datasource-proxy and H2, adds connection test

--- example/src/test/kotlin/dev/kounterspy/KounterspyApplicationTests.kt
+++ example/src/test/kotlin/dev/kounterspy/KounterspyApplicationTests.kt
@@ -1,13 +1,21 @@
 package dev.kounterspy
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 
 @SpringBootTest
-class KounterspyApplicationTests {
+class KounterspyApplicationTests(@Qualifier("namedParameterJdbcTemplate") val jdbc: NamedParameterJdbcTemplate) {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 
+    @Test
+    fun testConnection() {
+        val actual: List<Int> = jdbc.query("SELECT 1") { rs, _ -> rs.getInt(1) }
+        Assertions.assertEquals(listOf(1), actual)
+    }
 }
